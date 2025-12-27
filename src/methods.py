139313901cbbe1ba2cdb6a19748238bd38b3d9e8
AryanB-07 <137383071+AryanB-07@users.noreply.github.com>
2025-12-28 01:27:58 +0400

    Update methods.py

--- src/methods.py
+++ src/methods.py
@@ -317,7 +317,12 @@ class SSD(ApplyK):
         actual_iters = self.opt.train_iters
         self.opt.train_iters = len(train_loader) + len(forget_loader)
         time_start = time.process_time()
-        self.best_model = ssd_tuning(self.model, forget_loader, self.opt.SSDdampening, self.opt.SSDselectwt, train_loader, self.opt.device)
+        self.best_model = ssd_tuning(self.model, forget_loader, self.opt.SSDdampening, 
+                                      self.opt.SSDselectwt, train_loader, self.opt.device)
+        
+        # FIX: Actually update self.model
+        self.model.load_state_dict(self.best_model.state_dict())
+        
         self.save_files['train_time_taken'] += time.process_time() - time_start
         self.opt.train_iters = actual_iters
         return
